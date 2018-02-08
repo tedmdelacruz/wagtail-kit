3aba1fdbc585eeb4b809e1601d7fbc1302b16586
Ted Mathew dela Cruz <tedmdelacruz@gmail.com>
2018-02-08 21:40:28 +0800

    Fix DB settings

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -98,7 +98,7 @@ DATABASES = {
         'DATABASE': os.getenv('MYSQL_DATABASE'),
         'USER': os.getenv('MYSQL_USER'),
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'wagtail_db',
+        'HOST': 'db',
         'PORT': '3306'
     }
 }
